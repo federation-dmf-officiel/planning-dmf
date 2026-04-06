f18d2aa67fa25baa7a8f08dea98d019d59ba930f
danilou01 <rdan9646@gmail.com>
2026-04-06 14:33:16 +0200

    Remove test meeting from SHARED_EVENTS
    Removed a test meeting entry from SHARED_EVENTS.

--- data.js
+++ data.js
@@ -1,15 +1,5 @@
 const SHARED_EVENTS = [
-    {
-        "id": 1712345678910,
-        "title": "Réunion de test",
-        "date": "2024-06-15",
-        "time": "14:30",
-        "organizer": "Jean Dupont",
-        "type": "reunion",
-        "lat": 48.8566,
-        "lng": 2.3522,
-        "region": "Île-de-France"
-    }
+    
 ];
 
 const SHARED_ARCHIVES = [];
